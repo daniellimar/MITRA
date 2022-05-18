b866f70e65e42dcd16dff61124c75773d56f056c
Daniel Rodrigues de Lima <daniel.engca@outlook.com>
2022-05-18 15:10:31 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!--
+  DANIEL RODRIGUES DE LIMA
+  Cuiabá, MT - BRASIL
+
+  18/05/2022 - Início: 13:00 - Fim: 14:58
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- CSS (folha de estilos principal) -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Usuários</title>
+  </head>
+  <body>
+
+    <div class="container p-5 mt-5">
+      <div class="row justify-content-md-center">
+        <div class="row col-md-8 ">
+          <h1 class="display-5">Usuários</h1>
+          <div class="table-responsive">
+            <table class="table table-hover table-striped">
+              <thead class="font-weight-bold">
+                <tr>
+                  <th class="font-weight-bold" scope="col">Nome</th>
+                  <th class="font-weight-bold" scope="col">Telefone</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Joao dos Santos</th>
+                  <td>99999999</td>
+                </tr>
+                <tr>
+                  <th scope="row">Joao dos Santos</th>
+                  <td>99999999</td>
+                </tr>
+                <tr>
+                  <th scope="row">Joao dos Santos</th>
+                  <td>99999999</td>
+                </tr>
+                <tr>
+                  <th scope="row">Joao dos Santos</th>
+                  <td>99999999</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div class="card">
+              <div class="card-header">
+                <h5 class="font-weight-bold">Joao dos Santos</h5>
+              </div>
+              <div class="card-body mt-2 info">
+                <div class="container">
+                  <div class="row">
+
+                    <div class="col-md-4">
+                      <h6 class="font-weight-bold text-right text-rigth">Telefone:</h6>
+                      <h6 class="font-weight-bold text-right">Endereço:</h6>
+                      <h6 class="font-weight-bold text-right">Cidade:</h6>
+                    </div>
+
+                    <div class="col-md-4 infos">
+                        <span class="ml-1 text-left">9999999999</span><br>
+                        <span class="ml-1 text-left">Rua AVCDEF, n. y</span><br>
+                        <span class="ml-1 text-left">Curitiba</span><br>
+                      <h6 class="font-weight-bold text-right">
+                    </div>
+
+                    <div class="col-md-3">
+                      <h6 class="font-weight-bold text-right mt-5">Estado: <span class="ml-3 text-left" style="font-weight: 400;">PR</span></h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
+<!--
+  DANIEL RODRIGUES DE LIMA
+  Cuiabá, MT - BRASIL
+
+  18/05/2022 - Início: 13:00 - Fim: 14:58
+-->
